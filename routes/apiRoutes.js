0f935f0f816d8fafac552021a5089928ffad158d
alexbrandon3 <alexlbrandon@gmail.com>
2021-12-14 18:09:57 -0500

    added .env file

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,35 +1,44 @@
 const db = require("../models");
 
-module.exports = function(app) {
-    app.get("/api/workouts", (req, res) => {
-        db.Workout.find({}).then(dbWorkout => {
-            res.json(dbWorkout);
-        })
-        .catch(err => {
-            res.status(400).json(err);
-        });
-    })
-    app.get("/api/workouts/range", ({}, res) => {
-      db.Workout.find({}).then((dbWorkout) => {
-        res.json(dbWorkout);
-      }).catch(err => {
+module.exports = function (app) {
+  app.get("/api/workouts", (req, res) => {
+    db.Workout.aggregate([{$addFields: {totalDuration: { $sum: "$exercises.duration" }}}])
+      .then((workout) => {
+        res.json(workout);
+      })
+      .catch((err) => {
         res.status(400).json(err);
       });
-    });
-    app.post("/api/workouts/", (req, res) => {
-        db.Workout.create(req.body).then((dbWorkout) => {
-          res.json(dbWorkout);
-        }).catch(err => {
-            res.status(400).json(err);
-          });
+  });
+  app.get("/api/workouts/range", ({}, res) => {
+    db.Workout.aggregate([{$addFields: {totalDuration: { $sum: "$exercises.duration" }}}])
+      .then((workout) => {
+        res.json(workout);
+      })
+      .catch((err) => {
+        res.status(400).json(err);
       });
-      app.put("/api/workouts/:id", (req, res) => {
-        db.Workout.findByIdAndUpdate(
-          { _id: req.params.id }, { exercises: req.body }
-        ).then((dbWorkout) => {
-          res.json(dbWorkout);
-        }).catch(err => {
-          res.status(400).json(err);
-        });
-    });
+  });
+  app.post("/api/workouts/", (req, res) => {
+    db.Workout.create(req.body)
+      .then((workout) => {
+        res.json(workout);
+      })
+      .catch((err) => {
+        res.status(400).json(err);
+      });
+  });
+  app.put("/api/workouts/:id", (req, res) => {
+    db.Workout.findByIdAndUpdate(
+       req.params.id,
+      { exercises: req.body },
+      { runValidators: true}
+    )
+      .then((workout) => {
+        res.json(workout);
+      })
+      .catch((err) => {
+        res.status(400).json(err);
+      });
+  });
 };
--- server.js
+++ server.js
@@ -9,10 +9,7 @@ app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout");
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 app.listen(PORT, () => {
